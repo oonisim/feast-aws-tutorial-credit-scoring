6b632bc670502340cdf99e943cef3653d07bea61
onishima <masayuki.onishi@cba.com.au>
2025-08-25 17:09:09 +1000

    Initial

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 cd "$(realpath dirname $0)" || exit
-sudo apt install python3-dev postgresql-client libpq-dev
+sudo apt install python3.11 python3-dev build-essential libssl-dev libffi-dev python3.11-dev python3-pip python3.11-venv
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+
 pip install -r requirement.txt
 
 # For Postgresql psycopg2.
 # python3-dev does not work with the error (./psycopg/psycopg.h:35:10: fatal error: Python.h: No such file or directory)
 # #include <Python.h>. Must be python<version>-dev.
-pip install python3.11-dev libpq-dev psycopg2-binary psycopg2
+sudo apt install  postgresql-client libpq-dev
+pip install psycopg2-binary psycopg2
